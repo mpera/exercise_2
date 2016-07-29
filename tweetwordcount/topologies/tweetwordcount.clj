26c3dd9ba4522f2a352b79b485dae6ef528f9962
mpera <megan.pera@gmail.com>
2016-07-28 19:48:45 -0700

    Tweaked wording

--- tweetwordcount/topologies/tweetwordcount.clj
+++ tweetwordcount/topologies/tweetwordcount.clj
@@ -1,32 +1,34 @@
-(ns tweetwordcount
-  (:use     [streamparse.specs])
-  (:gen-class))
+(ns tweetcount
+        (:use     [streamparse.specs])
+        (:gen-class))
 
 (defn tweetwordcount [options]
-   [
-    ;; spout configuration
-    {"tweet-spout" (python-spout-spec
-          options
-          "spouts.tweets.Tweets"
-          ["tweet"]
-          :p 1
-          )
-    }
-    ;; bolt configuration
-    {"parse-tweet-bolt" (python-bolt-spec
-          options
-          {"tweet-spout" :shuffle}
-          "bolts.parse.ParseTweet"
-          ["word"]
-          :p 2
-          )
-     "count-bolt" (python-bolt-spec
-          options
-          {"parse-tweet-bolt" ["word"]}
-          "bolts.wordcount.WordCounter"
-          ["word" "count"]
-          :p 2
-          )
-    }
+        [
+        ;; spout configuration
+        {"tweet-spout" (python-spout-spec
+                options
+                "spouts.tweets.Tweets"
+                ["tweet"]
+                :p 1
+                 )
+        }
+        ;; bolt configuration
+        {"parse-tweet-bolt" (python-bolt-spec
+                options
+                {"tweet-spout" :shuffle}
+                "bolts.parse.ParseTweet"
+                ["valid_words"]
+                :p 2
+                 )
+
+        "count-bolt" (python-bolt-spec
+                options
+                {"parse-tweet-bolt" ["valid_words"]}
+                "bolts.wordcount.WordCounter"
+                ["word" "count"]
+                :p 2
+                )
+        }
+
   ]
 )
