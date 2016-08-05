b3a6d2575db8803097005ab7a1e2edf06920d18c
mpera <megan.pera@gmail.com>
2016-08-05 18:38:12 -0500

    Update finalresults.py

--- finalresults.py
+++ finalresults.py
@@ -1,10 +1,5 @@
-# This script gets a word as an argument and returns the total number of word occurrences in the stream. For example:
-# $ python finalresults.py hello
-# $ Total number of occurences of “hello”: 10
-
-# Running finalresults.py without an argument returns all the words in the stream and their total count of occurrences, sorted alphabetically in an ascending order, one word per line. For example:
-# $ python finalresults.py
-# $ (<word1>, 2), (<word2>, 8), (<word3>, 6), (<word4>, 1), 
+# This script gets a word as an argument and returns the total number of word occurrences in the stream.
+# Running finalresults.py without an argument returns all the words in the stream and their total count of occurrences, sorted alphabetically in an ascending order, one word per line.
 
 # Connect to postgres on the instance first 
 
